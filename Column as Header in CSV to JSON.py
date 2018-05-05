c30018358d7750f6ad113cac55193f96e9996db8
sumitlepcha <sumitlepcha@gmail.com>
2018-05-05 22:39:30 +0530

    Update Column as Header in CSV to JSON.py

--- Column as Header in CSV to JSON.py
+++ Column as Header in CSV to JSON.py
@@ -1,1 +1,26 @@
+import csv
 
+f = open("Column Header.csv")
+
+#read the file object
+reader = csv.reader(f)
+
+for row in reader:
+    col_count = len(row)
+columncount = col_count
+print(columncount)
+
+
+filereader = open("Column Header.csv")
+
+readfile = csv.reader(filereader)
+rows = list(readfile)
+
+i=1
+while i < columncount:
+    print("{")
+    for row in rows:
+        print('"',row[0],'"',':','"',row[i],'"')
+        
+    print("}")
+    i = i+1
